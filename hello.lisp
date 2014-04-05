e95f2f8d79297418e61bfde928f0e87bad2bd558
dow <dowdrake@msn.com>
2014-04-05 08:09:34 -0700

    fixed bug in sum-gen, added my-map

--- hello.lisp
+++ hello.lisp
@@ -9,3 +9,17 @@
 (defun fib (n)
   (next-fib n 0 1))
 
+(defun sum-gen (lo hi mmap next)
+  (if (> lo hi)
+      0
+      (+ 
+       (funcall mmap lo)
+       (sum-gen (funcall next lo) hi mmap next))))
+
+(defun my-map (f x)
+  (if (null x)
+      x
+      (cons (funcall f (car x))
+	    (my-map f (cdr x)))))
+
+
